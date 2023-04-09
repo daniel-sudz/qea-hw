185395fe8cd89eacc1b542a115944e025d12c0a9
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-04-08 22:24:38 -0400

    pretty decent results

--- ransack/multisack.m
+++ ransack/multisack.m
@@ -4,13 +4,16 @@ function [fit_segs_start, fit_segs_end] = multisack(scan_data, d, g, n)
     fit_segs_end = [];
     outliers = scan_data;
     % try to process model for some number of iterations
-    for iter=1:20
+    for iter=1:100
         % new model found
         [fit_seg_start, fit_seg_end, fit_inliers, fit_outliers] = sack(outliers, d, g, n);
         if(size(fit_inliers, 1) > 1)
             fit_segs_start = [fit_segs_start; fit_seg_start(1) fit_seg_start(2)];
             fit_segs_end = [fit_segs_end; fit_seg_end(1) fit_seg_end(2)];
             outliers = fit_outliers;
+
+            % debug plots, comment out for prod code
+            scatter(fit_inliers(:,1), fit_inliers(:,2));
         end
         % processed all
         if(size(outliers, 1) == 0)
