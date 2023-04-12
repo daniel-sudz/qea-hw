10493777436016954ddaa5633cf932032fc99457
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-04-12 11:29:05 -0400

    prelimenary not crashing on model

--- ransack/SackState.m
+++ ransack/SackState.m
@@ -5,7 +5,22 @@ classdef SackState < handle
       fit_segs_end = []; 
       fit_circle_origins = []; 
       fit_circle_radii = [];
+   end
+   properties(Constant)
       debug_mode = 1; % whether to plot or not while iterating
+
+      % RANSACK LINE FIT --------------------------------------------------
+      line_vertical_thresh = 0.2; % inlier threhold for vertical line fit
+      line_horizontal_gap_thresh = 0.2; % inlier threshold for line fit gap
+      line_num_iters = 10000; % how many times to sample for line fit
+      % RANSACK LINE FIT --------------------------------------------------
+
+      % RANSACK CIRCLE FIT --------------------------------------------------
+      circle_distance_thresh = 0.2; % inlier threshold for distance to circle model
+      circle_min_pints = 5; % minimum point requirements for circle model
+      circle_num_iters = 10000; % how many times to sample for circle fit
+      % RANSACK CIRCLE FIT --------------------------------------------------
+
    end
    methods(Static)
        % initialize ransack from polar coords
@@ -20,8 +35,8 @@ classdef SackState < handle
       function obj = SackState()
       end
       function [] = sack_iter(self)
-            [fit_seg_start, fit_seg_end, line_fit_inliers, line_fit_outliers] = sack(self.outliers, d, g, n);
-            [fit_circle_start, fit_circle_rad, circle_fit_inliers, circle_fit_outliers] = sack_circle(self.outliers, 0.2, 5, 10000);
+            [fit_seg_start, fit_seg_end, line_fit_inliers, line_fit_outliers] = sack(self.outliers, SackState.line_vertical_thresh, SackState.line_horizontal_gap_thresh, SackState.line_num_iters);
+            [fit_circle_start, fit_circle_rad, circle_fit_inliers, circle_fit_outliers] = sack_circle(self.outliers, SackState.circle_distance_thresh, SackState.circle_min_pints, SackState.circle_num_iters);
             if(size(line_fit_inliers,1) >= size(circle_fit_inliers,1))
                % line has a better fit
                self.fit_segs_start = [self.fit_segs_start; fit_seg_start(1) fit_seg_start(2)];
@@ -30,8 +45,7 @@ classdef SackState < handle
                
                % visualize output fit to plot if in debug mode
                if(self.debug_mode)
-                   plot([fit_seg_start(1), fit_sef_end(1)], [fit_seg_start(2), fit_sef_end(2)]);
-                   plot_circle(fit_circle_start(1), fit_circle_start(2), fit_circle_rad);
+                   plot([fit_seg_start(1), fit_seg_end(1)], [fit_seg_start(2), fit_seg_end(2)]);
                end
             else
                % circle has a better fit
