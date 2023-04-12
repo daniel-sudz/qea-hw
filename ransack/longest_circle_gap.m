8d4e8930d211b78716a1ea745102e2a654b8dca3
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-04-12 13:49:35 -0400

    use atan2 for angle gap calc

--- ransack/longest_circle_gap.m
+++ ransack/longest_circle_gap.m
@@ -3,11 +3,8 @@ function [distance] = longest_circle_gap(circle_origin, inlier_points)
     % convert to vector form
     inlier_points = map_by_row(inlier_points, @(point) (point - circle_origin'));
 
-    % horizontal vector 
-    horizontal_vector = [0; 1];
-
     % calculate angles
-    angles =  map_by_row(inlier_points, @(point) (acos((point * horizontal_vector) ./ (norm(point)*norm(horizontal_vector)))));
+    angles =  map_by_row(inlier_points, @(point) (atan2(point(2),point(1))));
 
     % find max gap 
     distance = max(diff(sort(angles))); 
