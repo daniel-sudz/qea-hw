0c8e29e1f2d2c406750c2026441383fee24117c5
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-04-13 09:03:09 -0400

    add code for colormap

--- ransack/SackState.m
+++ ransack/SackState.m
@@ -5,9 +5,13 @@ classdef SackState < handle
       fit_segs_end = []; 
       fit_circle_origins = []; 
       fit_circle_radii = [];
+      
+      % contour plot tool
+      mesh_grid_x = [];
+      mesh_grid_y = [];
    end
    properties(Constant)
-      debug_mode = 1; % whether to plot or not while iterating
+      debug_mode = 1; % whether to Wlot or not while iterating   
 
       % RANSACK LINE FIT --------------------------------------------------
       line_vertical_thresh = 0.1; % inlier threhold for vertical line fit
@@ -34,11 +38,16 @@ classdef SackState < handle
    end
    methods
       % constructor
-      function obj = SackState()
+      function self = SackState()
+         [x, y] = meshgrid(-3:0.01:3,-3:0.01:3);
+         self.mesh_grid_x = x;
+         self.mesh_grid_y = y;
+
       end
       function [model_found] = sack_iter(self)
             [fit_seg_start, fit_seg_end, line_fit_inliers, line_fit_outliers] = sack_line(self.outliers, SackState.line_vertical_thresh, SackState.line_horizontal_gap_thresh, SackState.line_min_pints, SackState.line_num_iters);
             [fit_circle_start, fit_circle_rad, circle_fit_inliers, circle_fit_outliers] = sack_circle(self.outliers, SackState.circle_distance_thresh, SackState.circle_gap_threshold_degrees, SackState.circle_min_pints, SackState.circle_num_iters);
+            circle_fit_inliers =[];
             if(size(line_fit_inliers,1) == 0 && size(circle_fit_inliers,1) == 0)
                 % no suitable model left in data
                 model_found = 0;
@@ -76,10 +85,20 @@ classdef SackState < handle
       function [] = sack_multi(self)
           % visualize output fit to plot if in debug mode
           if(self.debug_mode)
-              figure(); hold on; axis equal;
+              figure(); hold on; axis equal; shading interp;
           end
           % resolve iteratively
           while(sack_iter(self))
+          end
+          % plot out contour if in debug mode
+          if(self.debug_mode)
+              v = log(sqrt(self.mesh_grid_x.^2+ self.mesh_grid_y.^2));
+              contourf(self.mesh_grid_x,self.mesh_grid_y, v, 100, 'edgecolor','none');
+              colormap('hot');
+              % send contour plot to background by flipping plot elements
+              % https://www.mathworks.com/matlabcentral/answers/30212-how-to-bring-a-plot-to-the-front-or-back-among-multiple-plots
+              h = get(gca,'Children');
+              set(gca,'Children',flip(h))
           end
       end
    end
